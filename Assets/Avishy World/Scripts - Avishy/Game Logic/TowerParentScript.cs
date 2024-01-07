d3a7e830c05f59a73adde85190f6b305953e08e0
Avishy Menashe <Avishy.menashe@gmail.com>
2024-01-07 23:28:33 +0200

    towers + Tower lock on

--- Assets/Avishy World/Scripts - Avishy/Game Logic/TowerParentScript.cs
+++ Assets/Avishy World/Scripts - Avishy/Game Logic/TowerParentScript.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class TowerParentScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform currentTarget;
+    [SerializeField] private float range = 15;
+    [SerializeField] private Transform partToRotate;
+    [SerializeField] private float rotationSpeed = 15;
+    [SerializeField] private LayerMask enemyLayerMask;
+
+
+    private void Update()
     {
-        
+        UpdateTarget();
+        if (currentTarget == null) return;
+
+        //locking on target
+        Vector3 direction = currentTarget.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * rotationSpeed).eulerAngles;
+
+        partToRotate.rotation = Quaternion.Euler(0, rotation.y, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateTarget()
     {
-        
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, range, enemyLayerMask);
+
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (Collider enemy in hitColliders)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy.gameObject;
+            }
+        }
+
+        if(nearestEnemy && shortestDistance <= range)
+        {
+            currentTarget = nearestEnemy.transform;
+        }
+        else
+        {
+            currentTarget = null;
+        }
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
